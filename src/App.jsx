cd74978408927cf62019ea65d6b0753dcca79709
Mahbuba-S-Ety <sultanamahbuba09@gmail.com>
2026-03-14 15:52:07 +0600

    adding css styles to the JSX in three different ways and props

--- src/App.jsx
+++ src/App.jsx
@@ -10,16 +10,9 @@ function App() {
   return (
     <>
       <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
+   
         <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
+          <h1>React Core Concept</h1>
         </div>
         <button
           className="counter"
@@ -32,21 +25,43 @@ function App() {
       <div className="ticks"></div>
 
 
-      <div className="ticks"></div>
-      <section id="spacer"></section>
+      <Person name="Sultana" prof="Web Developer"></Person>
+      <Person name="Ety" prof="Student"></Person>
+      <Student></Student>
 
       <Friends> </Friends>
       <Div></Div>
-    <Friends> </Friends>
+      <Friends> </Friends>
+     
     </>
   )
 }
 
 export default App
 
-function Friends(){
+function Person(props) {
+  console.log(props)
   return (
-    <ul>
+    <div style={{
+      color: 'green',
+      border: "2px solid green",
+      borderRadius: "10px"
+    }}>
+      <h4>Developer: Mahbuba {props.name}</h4>
+      <p>Age: 22</p>
+      <p>profession: {props.prof}</p>
+    </div>
+  )
+}
+
+function Friends() {
+  
+  const ulStyle = {
+    color: 'black',
+    backgroundColor: 'gray'
+  }
+  return (
+    <ul style={ulStyle}>
       <li>Lamia</li>
       <li>Samia</li>
       <li>Halima</li>
@@ -56,10 +71,21 @@ function Friends(){
 
 function Div() {
   return (
-    <div>
+    <div className='div'>
       <p>Para 1: Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde, consequuntur.
       </p>
       <p>Para 2: Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
     </div>
   )
 }
+
+function Student() {
+  return (
+    <div className='student'>
+    <p>Name: Mahbuba</p>
+      <p>Dept: Political Science</p>
+   </div>
+  )
+}
+
+
