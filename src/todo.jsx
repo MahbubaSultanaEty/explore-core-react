a1140889cd3765d2fa10c29c63a1acee2715beaf
Mahbuba-S-Ety <sultanamahbuba09@gmail.com>
2026-03-18 11:05:57 +0600

    6 ways of conditional endering in React

--- src/todo.jsx
+++ src/todo.jsx
@@ -9,16 +9,66 @@
 
 
 /*🔹 export & import (JS/React)
-
  export → একটি module থেকে function, variable বা component বাইরে পাঠানোর জন্য।
  import → অন্য module থেকে export করা জিনিসটা আনার জন্য।
 */
 
 // Conditionl Rendering in react
+/*
 export default function ToDo({task, isDone, time=0}) {
     if (isDone === true) {
         return <li>Done: {task} Duration: { time}</li>
     } else {
         return <li>Pending: {task}</li>
     }
+}
+ */
+
+
+// Conditional rendering 2: Ternery opeartor
+/* 
+export default function ToDO({ task, isDone, time = 0 }) {
+    return isDone ? <li>Done: {task}, Duration : {time} </li> : <li>Pending: { task}</li>
+}
+    */
+
+
+// Conditional rendering 3: && operator [in react && operator cehcks if the condition is true]
+/*
+export default function ToDo({ task, isDone, time = 0 }) {
+    return isDone && <li>isDone : {task}, Duration : { time}</li>
+}
+*/
+
+
+// Conditional rendering 4: || operator [in react the || operator checks if the condition is false or you can say it works as else operator]
+/*
+export default function ToDo({ task, isDone, time = 0 }) {
+    return isDone || <li>Pernding: { task}, </li>
+}
+*/
+
+
+// Conditional rendering 5:
+/*
+export default function ToDo({ task, isDone, time = 0 }) {
+    if (isDone === true) {
+        return <li>Done: {task}, Duration : {time }</li>
+    } else {
+        return null; //if the condition enters else block it wont give any output
+    }
+}
+*/
+
+
+// Conditional rendering 6: [Using Varitables]
+export default function ToDo({ task, isDone, time = 0 }) {
+    let listItem;
+
+    if (isDone) {
+        listItem = <li>Done: {task}, Duration: { time}</li>
+    } else {
+        listItem = <li>Not Done: {task}, Duration { time}</li>
+    }
+    return listItem;
 }
